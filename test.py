2c9a32d871d2010331e4e25f65cc64af2852d429
harleenkaurhk000-bit <harleenkaurhk000@gmail.com>
2025-11-17 20:29:09 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,30 @@
 print("Hello World version 2!")
 print("what is your name?")
+
+
+def menu():
+  print ("*" *30)
+  print("***Welcome to my system***")
+  print("Chose from the following options: ")
+  print("1. Register")
+  print("2. Login")
+  print("3. EXIT")
+  print("*" * 30)
+
+def main():
+  while True:
+    menu()
+     choice = input(">")
+    if choice == "1":
+      register_user()
+    elif choice == "2"
+     if login_user():
+       print("You have logged in!!")
+    else: 
+      print("Incorrect login!!")
+    elif choice == "3":
+    print("Goodbye!!")
+    break
+
+if__name__=="__":
+  main()
